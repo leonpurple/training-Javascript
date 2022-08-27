c4d52fc47e99f30b934816812e4adb4cf2eaa6e9
leonpurple <leonpurple.2903@gmail.com>
2022-08-27 18:44:31 +0200

    bucle while

---
+++ bucle-While.js
@@ -0,0 +1,17 @@
+// Bucles While
+
+let i = 0;
+let max = 10;
+while (i<10) {
+console.log( i)
+i++;
+}
+// tener cuidado con los bucles infinitos
+
+//Do..While
+i = 15 ;
+
+do{
+	console.log("Estoy en el do while")
+
+}while(i < max)
--- bucles for.js
+++ bucles for.js
@@ -23,4 +23,4 @@ let lista = [0,1,2,3,4,5,6,7,8,9,]; //acceder a la lista con un for
 for(let i= 0; i < lista.length; i++){
 	console.log(lista)
 
-}
+} 
