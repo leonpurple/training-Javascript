e4b95386818862438072dc6706888b6a07f0c2bf
leonpurple <leonpurple.2903@gmail.com>
2022-09-23 22:07:38 +0200

    variables numericas

---
+++ variables-numericas.js
@@ -0,0 +1,18 @@
+//secion 05 - NUMBERS
+
+//declaracion de variables numericas (enteros y flotantes)
+
+let a = 5
+console.log(a)
+//--------------
+
+let b = 0.1
+console.log(b)
+//--------------
+
+//precición y truco para obtener valores reales git
+let c = 0.2
+
+console.log(b+c)
+
+console.log(Math.round((0.1 + 0.2)* 100)/100)
