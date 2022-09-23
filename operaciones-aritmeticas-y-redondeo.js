a61555a0e2f18531e9329effe80950c5db3484bb
leonpurple <leonpurple.2903@gmail.com>
2022-09-23 22:40:50 +0200

    redondeo

---
+++ operaciones-aritmeticas-y-redondeo.js
@@ -0,0 +1,55 @@
+let b = 4.8
+let a = 2.1
+
+//SUMA (+)
+console.log(a + b )
+
+//RESTA
+console.log(a-b)
+
+//MULTIPLICACIÖN
+console.log(a*b)
+
+//DIVISIÓN
+console.log(a/b)
+
+//REPRESENTACIÓN DE LOS NUMEROS EN CADENA DE TEXTO
+console.log(typeof a)
+let a_s = a.toString()
+
+console.log(a_s)
+console.log(typeof a_s)
+
+//REDONDEADO DE NUMEROS DECIMALES 
+
+let c = 3.3
+let d = c * 3
+//toFixed (limita el numero de decimales)
+console.log(d)
+console.log(typeof d)
+console.log(d.toFixed())
+console.log(d.toFixed(0))
+console.log(d.toFixed(1))
+console.log(d.toFixed(2))
+console.log(d.toFixed(3))
+console.log(typeof d.toFixed(3))
+
+let e = 1978.123456789
+let ff = 197
+console.log(e.toFixed(3))
+console.log(ff.toFixed(2))
+console.log(typeof ff.toFixed(2))
+
+//toPrecision(x)
+
+console.log(e.toPrecision(4))
+console.log(e.toPrecision(5))
+console.log(e.toPrecision(6))
+console.log(e.toPrecision(7))
+console.log(e.toPrecision(8))
+console.log(typeof e.toPrecision(8))
+
+
+
+
+
